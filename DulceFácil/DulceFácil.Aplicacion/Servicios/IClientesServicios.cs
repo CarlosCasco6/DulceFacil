e4f65291c31e55066706a3dfd818286520e40697
Carlos <carlosc97@outlook.es>
2025-06-23 21:50:11 -0500

    Creacion de Interfaces y Clases en el modulo de Aplicacion

---
+++ DulceFácil/DulceFácil.Aplicacion/Servicios/IClientesServicios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using DulceFácil.Infraestructura.AccesoDatos;
+
+namespace DulceFácil.Aplicacion.Servicios
+{
+    [ServiceContract]
+
+    internal interface IClientesServicios
+    {
+        [OperationContract]
+        Task AddClienteAsync(Clientes nuevoCliente);
+
+        [OperationContract]
+        Task UpdateClienteAsync(Clientes Entity);//actualizar
+        [OperationContract]
+        Task DeleteClienteAsync(int id);//eliminar por ID
+        [OperationContract]
+        Task<IEnumerable<Clientes>> GetAllClienteAsync();//listar todo
+        [OperationContract]
+        Task<Clientes> GetByIdClienteAsync(int id);//buscar por ID
+    }
+}
+
---
+++ DulceFácil/DulceFácil.Aplicacion/ServicosImpl/ClientesServiciosImpl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DulceFácil.Dominio.Modelos.Abstracciones;
+using DulceFácil.Infraestructura.AccesoDatos;
+using DulceFácil.Infraestructura.AccesoDatos.Repositorio;
+
+namespace DulceFácil.Aplicacion.ServicosImpl
+{
+    internal class ClientesServiciosImpl
+    {
+        private IClientesRepositorio clientesRepositorio;
+
+        public ClientesServiciosImpl(DulceFacilDBContext _dulcefacilDBContext)
+        {
+            this.clientesRepositorio = new ClientesRepositorioImpl(_dulcefacilDBContext);
+        }
+
+        public async Task AddClienteAsync(Clientes nuevoCliente)
+        {
+            await clientesRepositorio.AddAsync(nuevoCliente);
+        }
+
+        public async Task DeleteClienteAsync(int id)
+        {
+            await clientesRepositorio.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<Clientes>> GetAllClienteAsync()
+        {
+            return await clientesRepositorio.GetAllAsync();
+        }
+
+        public async Task<Clientes> GetByIdClienteAsync(int id)
+        {
+            return await clientesRepositorio.GetByIdAsync(id);
+        }
+
+        public async Task UpdateClienteAsync(Clientes Entity)
+        {
+            await clientesRepositorio.UpdateAsync(Entity);
+        }
+    }
+}
